93dbc25fc0afa19b225eaf9ca6060832bcf7cfa6
Purnell Jones <Purnell_Jones@ultimatesoftware.com>
2018-02-27 17:15:13 -0500

    Initial Comment

---
+++ app/slack_bot.py
@@ -0,0 +1,8 @@
+from slackclient import SlackClient;
+
+slack_client = SlackClient("token");
+
+print(help(slack_client));
+
+
+# https://www.youtube.com/watch?v=QFPT37NoALA
---
+++ test/slack_bot_test.py
@@ -0,0 +1,5 @@
+import pytest;
+import slack_bot import *;
+
+def test_slackConnect():
+    print(slackConnect());
