500de80a01de3d6b5458741b410e4f28e185e2c6
jonepl <jonepl.ufl@gmail.com>
2018-03-04 22:09:46 -0500

    Found slack tutorial for python :)

--- app/slackBot.py
+++ app/slackBot.py
@@ -1,7 +1,25 @@
 from slackclient import SlackClient
 import config.creds
 
-slack_client = SlackClient()
+
+# For all Slack bot methods
+class SlackBot():
+
+    def __init__(self, token):
+        self.slack_client = SlackClient()
+
+    def readReadRTM():
+        print(self.slack_client.rtm_read());
+        time.sleep(1);
+
+    def getBotID():
+        pass;
+
+    def getSlackInput():
+        pass;
+
+    def writeToSlack(channel, message):
+        pass;
 
 def slackConnect():
     return slack_client.rtm_connect(config.slack['token']);
