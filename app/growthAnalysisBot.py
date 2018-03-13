6e8ae80d801787b8fd17c69763859eabf499c089
jonepl <jonepl.ufl@gmail.com>
2018-03-12 22:41:20 -0400

    Attempting to make bot echo messages

--- app/growthAnalysisBot.py
+++ app/growthAnalysisBot.py
@@ -12,22 +12,46 @@ class GrowthAnalysisBot(SlackBot) :
         while(True) :
             rawInput = super().readChannels();
             print(rawInput);
+            # print("~~~~~~~~~~~~");
+            # self.parseRawInput(rawInput);
             time.sleep(1);
 
     # Overriden method
-    def parseRawInput(self):
+    def parseRawInput(self, rawInput):
         result = [];
-        if(len(input) > 0 and 'text' in input[0]) :
-            input = input[0];
-            print(input);
-            user = input['user'];
-            message = input['text'];
-            channel = input['channel'];
-            result = [str(user), str(message), str(channel)]
-            print(result);
-            return result;
-        else :
-            return result;
+        if (not self.isEmpty(rawInput)) :
+            if(self.isMessage(rawInput)) :
+                result = self.parseMessage(rawInput);
+                self.determineAction(result);
+        # if(len(rawInput) > 0 and 'text' in rawInput[0]) :
+        #     rawInput = rawInput[0];
+        #     user = rawInput['user'];
+        #     message = rawInput['text'];
+        #     channel = rawInput['channel'];
+        #     result = [str(user), str(message), str(channel)]
+        #     print(result);
+
+    def isTyping(self, rawInput) :
+        if(rawInput[0].get('type') == 'user_typing') : return True;
+        else : return False
+
+    def isEmpty(self, rawInput) :
+        if not rawInput : return True;
+        else : return False
+
+    def isMessage(self, rawInput) :
+        if(rawInput[0].get('type') == 'message') : return True;
+        else : return False;
+
+    def parseMessage(self, rawInput) :
+        rawInput = rawInput[0];
+        user = rawInput['user'];
+        message = rawInput['text'];
+        channel = rawInput['channel'];
+        return { 'user' : str(user), 'message' : str(message), 'channel' : str(channel) };
+
+    def determineAction(self, cleanedInput) :
+        self.writeToSlack(cleanedInput['channel'], cleanedInput['message']);
 
 if __name__ == '__main__':
     from slackBot import SlackBot;
