ea50675b2fd91a8c31b5f3e4017e7a513ca359d2
Purnell Jones <Purnell_Jones@ultimatesoftware.com>
2018-03-14 12:55:41 -0400

    Experiencing issues with making bot echoing a user's message.

--- app/growthAnalysisBot.py
+++ app/growthAnalysisBot.py
@@ -6,30 +6,38 @@ class GrowthAnalysisBot(SlackBot) :
 
     def __init__(self, token):
         super().__init__(token);
-        super().connect(self.token);
+        self.connect(self.token);
 
     def run(self) :
+        #self.writeToSlack('D9GCAPGNL', 'Testing');
         while(True) :
-            rawInput = super().readChannels();
+            rawInput = self.readChannels();
             print(rawInput);
             # print("~~~~~~~~~~~~");
-            # self.parseRawInput(rawInput);
+            self.handle(rawInput);
             time.sleep(1);
 
     # Overriden method
-    def parseRawInput(self, rawInput):
+    def handle(self, rawInput):
         result = [];
-        if (not self.isEmpty(rawInput)) :
+        if(not self.isEmpty(rawInput)) :
             if(self.isMessage(rawInput)) :
-                result = self.parseMessage(rawInput);
-                self.determineAction(result);
-        # if(len(rawInput) > 0 and 'text' in rawInput[0]) :
-        #     rawInput = rawInput[0];
-        #     user = rawInput['user'];
-        #     message = rawInput['text'];
-        #     channel = rawInput['channel'];
-        #     result = [str(user), str(message), str(channel)]
-        #     print(result);
+                message = self.parseMessage(rawInput);
+                self.writeToSlack(message['channel'], message['message']);
+            elif (self.isTyping(rawInput)) :
+                pass;
+            elif (self.isHello(rawInput)) :
+                pass;
+            else :
+                print("Unknown input type");
+        return result;
+
+    def parseMessage(self, rawInput) :
+        rawInput = rawInput[0];
+        user = rawInput['user'];
+        message = rawInput['text'];
+        channel = rawInput['channel'];
+        return { 'user' : str(user), 'message' : str(message), 'channel' : str(channel) };
 
     def isTyping(self, rawInput) :
         if(rawInput[0].get('type') == 'user_typing') : return True;
@@ -43,15 +51,11 @@ class GrowthAnalysisBot(SlackBot) :
         if(rawInput[0].get('type') == 'message') : return True;
         else : return False;
 
-    def parseMessage(self, rawInput) :
-        rawInput = rawInput[0];
-        user = rawInput['user'];
-        message = rawInput['text'];
-        channel = rawInput['channel'];
-        return { 'user' : str(user), 'message' : str(message), 'channel' : str(channel) };
+    def isHello(self, rawInput) :
+        if(rawInput[0].get('type') == 'hello') : return True;
+        else : return False
 
-    def determineAction(self, cleanedInput) :
-        self.writeToSlack(cleanedInput['channel'], cleanedInput['message']);
+
 
 if __name__ == '__main__':
     from slackBot import SlackBot;
